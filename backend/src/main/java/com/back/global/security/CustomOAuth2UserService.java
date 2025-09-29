d6b54931161c046358a65f9faed7dd7047b5c623
sik2 <bickwang123@gmail.com>
2025-09-29 10:16:00 +0900

    17

---
+++ backend/src/main/java/com/back/global/security/CustomOAuth2UserService.java
@@ -0,0 +1,57 @@
+package com.back.global.security;
+
+
+import com.back.domain.member.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Map;
+
+@RequiredArgsConstructor
+@Service
+@Slf4j
+public class CustomOAuth2UserService extends DefaultOAuth2UserService {
+    private final MemberService memberService;
+
+    @Transactional
+    // 카카오톡 로그인이 성공할 때 마다 이 함수가 실행된다.
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        OAuth2User oAuth2User = super.loadUser(userRequest);
+
+        String oauthUserId = oAuth2User.getName();
+        String providerTypeCode = userRequest.getClientRegistration().getRegistrationId().toLowerCase();
+
+        Map<String, Object> attributes = oAuth2User.getAttributes();
+        Map<String, Object> attributesProperties = (Map<String, Object>) attributes.get("properties");
+
+        String userNicknameAttributeName = "nickname";
+        String profileImgUrlAttributeName = "profile_image";
+
+        String nickname = (String) attributesProperties.get(userNicknameAttributeName);
+        String profileImgUrl = (String) attributesProperties.get(profileImgUrlAttributeName);
+        String username = providerTypeCode + "__%s".formatted(oauthUserId);
+
+        String password = "";
+
+        // TODO: 회원가입 처리
+        // Member member = memberService.modifyOrJoin(username, password, nickname, profileImgUrl).data();
+
+        return new DefaultOAuth2User(List.of(),
+                attributes,
+                userRequest
+                        .getClientRegistration()
+                        .getProviderDetails()
+                        .getUserInfoEndpoint()
+                        .getUserNameAttributeName()
+        );
+    }
+}
